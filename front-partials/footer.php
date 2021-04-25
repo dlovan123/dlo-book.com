c09fa469ffb8c877b8ec408f8e202f23be1c49da
dlovan123 <61874149+dlovan123@users.noreply.github.com>
2021-04-26 01:12:50 +0300

    Add files via upload

---
+++ front-partials/footer.php
@@ -0,0 +1,21 @@
+    <!--social Section start-->
+    <section class="social text-center">
+        <div class="container">
+            <ul>
+                <li><a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a></li>
+                <li><a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a></li>
+                
+            </ul>
+        </div>
+    </section>
+    <!--social Section ends-->
+    <!--footer Section start-->
+    <section class="footer">
+        <div class="container">
+            <p class="text-center">All rights reseved.Designed By <a>Dlovan kheri</a></p>
+        </div>
+    </section>
+    <!--footer Section ends-->
+
+</body>
+</html>
---
+++ front-partials/menu.php
@@ -0,0 +1,26 @@
+<?php include('config/constans.php');?>
+<!DOCTYPE html>
+<head>
+    <title>Book-Store</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <!--menu Section start-->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo  SITEURL;?>"><img src="images/logo.jpg" alt="logo" class="img-responsive" /></a>
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li><a href="<?php echo SITEURL;?>">Home</a></li>
+                    <li><a href="<?php echo SITEURL;?>category.php">Category</a></li>
+                    <li><a href="<?php echo SITEURL;?>books.php">Books</a></li>
+
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!--menu Section ends-->
+    
